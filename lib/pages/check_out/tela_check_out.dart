db0915ac3f51d26db9a53a4fe711267a5f9e5d6f
Matheus Soares <matheusmatias2622@gmail.com>
2023-05-16 18:12:50 -0300

    Verificacao em check out implementada

--- lib/pages/check_out/tela_check_out.dart
+++ lib/pages/check_out/tela_check_out.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:rodarwebos/pages/check_in/variaveis_options.dart';
+import 'package:rodarwebos/pages/equipamentos/tela_equipamento.dart';
 import 'package:rodarwebos/pages/motivos/tela_relate_motivos.dart';
+import 'package:rodarwebos/widgets/botoes/botao_proximo.dart';
 import 'package:rodarwebos/widgets/check_in/container_check_in.dart';
 import 'package:rodarwebos/widgets/check_in/container_observacao_adicional.dart';
-import 'package:rodarwebos/widgets/check_out/container_check_out.dart';
 
 class CheckOutTela extends StatefulWidget {
   @override
@@ -10,6 +12,9 @@ class CheckOutTela extends StatefulWidget {
 }
 
 class _CheckOutTelaState extends State<CheckOutTela> {
+  SelectedOptions selectedOptions =
+      SelectedOptions(); // Instância da classe SelectedOptions
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,30 +26,164 @@ class _CheckOutTelaState extends State<CheckOutTela> {
             Navigator.pop(context);
           },
         ),
-        title: Text('Check-Out'),
+        title: Text('Check-out'),
       ),
       body: ListView(
         children: [
-          ContainerCheckOut(title: 'Luzes Painel Instrumento'),
-          ContainerCheckOut(title: 'Ar Condicionado'),
-          ContainerCheckOut(title: 'Ar quente / Ventilação'),
-          ContainerCheckOut(title: 'Rádio / CD / DVD / MP3'),
-          ContainerCheckOut(title: 'Buzinas'),
-          ContainerCheckOut(title: 'Teto / Painel / Quebra-sol'),
-          ContainerCheckOut(title: 'Partida e func. do motor'),
-          ContainerCheckOut(title: 'Vidros elétricos'),
-          ContainerCheckOut(title: 'Alarme'),
-          ContainerCheckOut(title: 'Condições instalação elétrica'),
-          ContainerObservacaoAdicional(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => (RelateMotivo())),
-              );
+          ContainerCheckIn(
+            title: 'Luzes Painel Instrumento',
+            onOptionSelected: (option) {
+              setState(() {
+                selectedOptions.luzesPainelInstrumento =
+                    option; // Atualize o valor selecionado
+              });
             },
           ),
+          ContainerCheckIn(
+            title: 'Ar Condicionado',
+            onOptionSelected: (option) {
+              setState(() {
+                selectedOptions.arCondicionado =
+                    option; // Atualize o valor selecionado
+              });
+            },
+          ),
+          ContainerCheckIn(
+            title: 'Ar quente / Ventilação',
+            onOptionSelected: (option) {
+              setState(() {
+                selectedOptions.arQuenteVentilacao =
+                    option; // Atualize o valor selecionado
+              });
+            },
+          ),
+          ContainerCheckIn(
+            title: 'Rádio / CD / DVD / MP3',
+            onOptionSelected: (option) {
+              setState(() {
+                selectedOptions.radioCdDvdMp3 =
+                    option; // Atualize o valor selecionado
+              });
+            },
+          ),
+          ContainerCheckIn(
+            title: 'Buzinas',
+            onOptionSelected: (option) {
+              setState(() {
+                selectedOptions.buzinas =
+                    option; // Atualize o valor selecionado
+              });
+            },
+          ),
+          ContainerCheckIn(
+            title: 'Teto / Painel / Quebra-sol',
+            onOptionSelected: (option) {
+              setState(() {
+                selectedOptions.tetoPainelQuebraSol =
+                    option; // Atualize o valor selecionado
+              });
+            },
+          ),
+          ContainerCheckIn(
+            title: 'Partida e func. do motor',
+            onOptionSelected: (option) {
+              setState(() {
+                selectedOptions.partidaFuncMotor =
+                    option; // Atualize o valor selecionado
+              });
+            },
+          ),
+          ContainerCheckIn(
+            title: 'Vidros elétricos',
+            onOptionSelected: (option) {
+              setState(() {
+                selectedOptions.vidrosEletricos =
+                    option; // Atualize o valor selecionado
+              });
+            },
+          ),
+          ContainerCheckIn(
+            title: 'Alarme',
+            onOptionSelected: (option) {
+              setState(() {
+                selectedOptions.alarme = option; // Atualize o valor selecionado
+              });
+            },
+          ),
+          ContainerCheckIn(
+            title: 'Condições instalação elétrica',
+            onOptionSelected: (option) {
+              setState(() {
+                selectedOptions.condicoesIntalacaoEletrico =
+                    option; // Atualize o valor selecionado
+              });
+            },
+          ),
+          ContainerObservacaoAdicional(
+            onPressed: () {
+              printSelectedOptions();
+              checkNavigation();
+              
+            },
+          ),
+          
         ],
       ),
     );
   }
+
+  void printSelectedOptions() {
+    print(
+        'Luzes Painel Instrumento: ${selectedOptions.luzesPainelInstrumento}');
+    print('Ar Condicionado: ${selectedOptions.arCondicionado}');
+    print('Ar Quente / Ventilação: ${selectedOptions.arQuenteVentilacao}');
+    print('Rádio / CD / DVD / MP3: ${selectedOptions.radioCdDvdMp3}');
+    print('Buzinas: ${selectedOptions.buzinas}');
+    print('Teto / Painel / Quebra-sol: ${selectedOptions.tetoPainelQuebraSol}');
+    print('Partida e func. do motor: ${selectedOptions.partidaFuncMotor}');
+    print('Vidros elétricos: ${selectedOptions.vidrosEletricos}');
+    print('Alarme: ${selectedOptions.alarme}');
+    print(
+        'Condições instalação elétrica: ${selectedOptions.condicoesIntalacaoEletrico}');
+    // Imprima outras opções selecionadas
+  }
+
+  void checkNavigation() {
+    if (selectedOptions.luzesPainelInstrumento.isEmpty ||
+        selectedOptions.arCondicionado.isEmpty ||
+        selectedOptions.arQuenteVentilacao.isEmpty ||
+        selectedOptions.radioCdDvdMp3.isEmpty ||
+        selectedOptions.buzinas.isEmpty ||
+        selectedOptions.tetoPainelQuebraSol.isEmpty ||
+        selectedOptions.partidaFuncMotor.isEmpty ||
+        selectedOptions.vidrosEletricos.isEmpty ||
+        selectedOptions.alarme.isEmpty ||
+        selectedOptions.condicoesIntalacaoEletrico.isEmpty) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Aviso'),
+            content: Text(
+                'Por favor, preencha todas as opções antes de prosseguir.'),
+            actions: [
+              TextButton(
+                child: Text('OK'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    } else {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => RelateMotivo()),
+      );
+    }
+  }
 }
+
+
--- lib/widgets/check_out/container_check_out.dart
+++ lib/widgets/check_out/container_check_out.dart
@@ -2,96 +2,105 @@ import 'package:flutter/material.dart';
 
 class ContainerCheckOut extends StatefulWidget {
   final String title;
+  final ValueChanged<String> onOptionSelected;
 
-  ContainerCheckOut({required this.title});
+  ContainerCheckOut({required this.title, required this.onOptionSelected});
 
   @override
   _ContainerCheckOutState createState() => _ContainerCheckOutState();
 }
 
 class _ContainerCheckOutState extends State<ContainerCheckOut> {
-  String selectedButton = '';
+  String? selectedButton; // Alterada para tipo nullable String
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       padding: EdgeInsets.all(16.0),
-      child: SingleChildScrollView(
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            border: Border.all(
-              color: Colors.grey[300]!,
-              width: 1.0,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(
+            color: Colors.grey[300]!,
+            width: 1.0,
+          ),
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              widget.title,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16.0,
+              ),
+              textAlign: TextAlign.center,
             ),
-            borderRadius: BorderRadius.circular(8.0),
-          ),
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Text(
-                widget.title,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16.0,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 5.0),
-              Column(
-                children: <Widget>[
-                  RadioListTile(
-                    title: Text(
-                      'OK',
-                      style: TextStyle(fontSize: 14.0),
+            SizedBox(height: 5.0),
+            StatefulBuilder(
+              builder: (context, setState) {
+                return Column(
+                  children: <Widget>[
+                    RadioListTile(
+                      title: Text(
+                        'OK',
+                        style: TextStyle(fontSize: 14.0),
+                      ),
+                      value: 'OK',
+                      groupValue: selectedButton,
+                      onChanged: (value) {
+                        setState(() {
+                          selectedButton = value.toString();
+                          widget.onOptionSelected(value
+                              .toString()); // Chame a função onOptionSelected e passe o valor selecionado
+                        });
+                      },
                     ),
-                    value: 'OK',
-                    groupValue: selectedButton,
-                    onChanged: (value) {
-                      setState(() {
-                        selectedButton = value.toString();
-                      });
-                    },
-                  ),
-                  RadioListTile(
-                    title: Text(
-                      'Defeito',
-                      style: TextStyle(fontSize: 14.0),
+                    RadioListTile(
+                      title: Text(
+                        'Defeito',
+                        style: TextStyle(fontSize: 14.0),
+                      ),
+                      value: 'Defeito',
+                      groupValue: selectedButton,
+                      onChanged: (value) {
+                        setState(() {
+                          selectedButton = value.toString();
+                          widget.onOptionSelected(value
+                              .toString()); // Chame a função onOptionSelected e passe o valor selecionado
+                        });
+                      },
                     ),
-                    value: 'Defeito',
-                    groupValue: selectedButton,
-                    onChanged: (value) {
-                      setState(() {
-                        selectedButton = value.toString();
-                      });
-                    },
-                  ),
-                  RadioListTile(
-                    title: Text(
-                      'Não Possui',
-                      style: TextStyle(fontSize: 14.0),
+                    RadioListTile(
+                      title: Text(
+                        'Não Possui',
+                        style: TextStyle(fontSize: 14.0),
+                      ),
+                      value: 'Não Possui',
+                      groupValue: selectedButton,
+                      onChanged: (value) {
+                        setState(() {
+                          selectedButton = value.toString();
+                          widget.onOptionSelected(value
+                              .toString()); // Chame a função onOptionSelected e passe o valor selecionado
+                        });
+                      },
                     ),
-                    value: 'Não Possui',
-                    groupValue: selectedButton,
-                    onChanged: (value) {
-                      setState(() {
-                        selectedButton = value.toString();
-                      });
-                    },
-                  ),
-                ],
+                  ],
+                );
+              },
+            ),
+            SizedBox(height: 16.0),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Observação...',
+                border: OutlineInputBorder(),
               ),
-              SizedBox(height: 16.0),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'Observação...',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
