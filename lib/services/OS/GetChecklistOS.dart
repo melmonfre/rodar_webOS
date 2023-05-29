6f5e0aea5671e4a346198a7e1aa2977de2d35349
Melissa Monfre Dos Santos <viniciusmonfre@linuxmail.org>
2023-05-29 12:09:20 -0300

    implementação do checklist da OS

---
+++ lib/services/OS/GetChecklistOS.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../Constantes/Urlconst.dart';
+
+class GetChecklistOS{
+  obter(var OSID) async {
+    SharedPreferences opcs = await SharedPreferences.getInstance();
+    var token = opcs.getString("token")!;
+    var headers = {
+      'Accept': 'application/json',
+      'Authorization': 'Bearer $token',
+    };
+
+    var url = Uri.parse('${Urlconst().url}ordem_servico/checklist/$OSID');
+    var res = await http.get(url, headers: headers);
+    if (res.statusCode != 200) throw Exception('http.get error: statusCode= ${res.statusCode}');
+    print(res.body);
+    return jsonDecode(res.body);
+
+  }
+}
