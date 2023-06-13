e88ed723d5f5fee24972dc70a528edf55b25c8ce
Matheus Soares <matheusmatias2622@gmail.com>
2023-06-13 19:49:59 -0300

    Rotas tipo de tela

--- lib/pages/equipamentos/tela_equipamento.dart
+++ lib/pages/equipamentos/tela_equipamento.dart
@@ -10,8 +10,16 @@ class Equipamentos extends StatefulWidget {
 
 class _EquipamentosState extends State<Equipamentos> {
   var variaveis = VariaveisResumo();
+  var control =
+      "manutenção"; // Definindo o tipo de tela - estatico somente para testes
+
   @override
   Widget build(BuildContext context) {
+    bool isManutencao = control == "manutenção";
+    bool isRetirada = control == "retirada";
+    bool isInstalacao = control == "instalação";
+    bool isTroca = control == "troca";
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -21,7 +29,7 @@ class _EquipamentosState extends State<Equipamentos> {
             Navigator.pop(context);
           },
         ),
-        title: Text('Equipamentos'),
+        title: Text(control),
       ),
       body: SingleChildScrollView(
         child: Padding(
@@ -40,11 +48,138 @@ class _EquipamentosState extends State<Equipamentos> {
                 ),
               ),
               SizedBox(height: 16.0),
-              ContainerEquipamento(),
+              if (isManutencao) ContainerManutencao(),
+              if (isRetirada) ContainerRetirada(),
+              if (isInstalacao) ContainerInstalacao(),
+              if (isTroca) ContainerTroca(),
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class ContainerTroca extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        // Input equipamento
+        TextFormField(
+          decoration: InputDecoration(
+            labelText: 'Equipamento',
+          ),
+        ),
+        // Input equipamento retirado
+        TextFormField(
+          decoration: InputDecoration(
+            labelText: 'Equipamento Retirado',
+          ),
+        ),
+        // Checkbox situação do equipamento
+        Row(
+          children: [
+            Checkbox(
+              value: true, // Valor do checkbox "ok"
+              onChanged: (value) {},
+            ),
+            Text('OK'),
+            Checkbox(
+              value: false, // Valor do checkbox "com defeito"
+              onChanged: (value) {},
+            ),
+            Text('Com Defeito'),
+          ],
+        ),
+        // Input local de instalação
+        TextFormField(
+          decoration: InputDecoration(
+            labelText: 'Local de Instalação',
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ContainerManutencao extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        // Input equipamento
+        TextFormField(
+          decoration: InputDecoration(
+            labelText: 'Equipamento',
+          ),
+        ),
+        // Input local de instalação
+        TextFormField(
+          decoration: InputDecoration(
+            labelText: 'Local de Instalação',
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ContainerRetirada extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        // Input equipamento
+        TextFormField(
+          decoration: InputDecoration(
+            labelText: 'Equipamento',
+          ),
+        ),
+        // Checkbox situação do equipamento
+        Row(
+          children: [
+            Checkbox(
+              value: true, // Valor do checkbox "ok"
+              onChanged: (value) {},
+            ),
+            Text('OK'),
+            Checkbox(
+              value: false, // Valor do checkbox "com defeito"
+              onChanged: (value) {},
+            ),
+            Text('Com Defeito'),
+          ],
+        ),
+        // Input local de instalação
+        TextFormField(
+          decoration: InputDecoration(
+            labelText: 'Local de Instalação',
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ContainerInstalacao extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        // Input equipamento
+        TextFormField(
+          decoration: InputDecoration(
+            labelText: 'Equipamento',
+          ),
+        ),
+        // Input local de instalação
+        TextFormField(
+          decoration: InputDecoration(
+            labelText: 'Local de Instalação',
+          ),
+        ),
+      ],
     );
   }
 }
